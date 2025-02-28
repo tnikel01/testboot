c904e1073eef69730d206b6b38f24e5700a47d14
tnikel01 <tnikel01@bvsd.org>
2025-02-28 21:09:27 +0000

    init commit

---
+++ src/main/java/org/fairviewhigh/javaprojekt/JavaprojektApplication.java
@@ -0,0 +1,13 @@
+package org.fairviewhigh.javaprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaprojektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fairviewhigh/javaprojekt/JavaprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fairviewhigh.javaprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
